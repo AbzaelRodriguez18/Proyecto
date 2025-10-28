117fda15e5677f29e947c61b81fa76294212db8a
AbzaelRodriguez18 <vyeah1165@gmail.com>
2025-10-28 20:34:58 +0100

    segundo commit

--- app/src/main/java/com/example/proyecto_personal/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_personal/MainActivity.kt
@@ -10,7 +10,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.json.JSONObject
+import java.net.URL
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +23,9 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun Conversor() {
-    var cantidad by remember { mutableStateOf("") }
-
+    var Monto by remember { mutableStateOf("") }
+    var resultado by remember { mutableStateOf("") }
+    val investigador = rememberCoroutineScope()
 
     Column(
         modifier = Modifier
@@ -31,14 +35,37 @@ fun Conversor() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text("USD → EUR",
-            fontSize = 16.sp)
+            fontSize = 20.sp)
 
         Spacer(Modifier.height(16.dp))
         TextField(
-            value = cantidad,
-            onValueChange = { cantidad = it },
+            value = Monto,
+            onValueChange = { Monto = it },
             label = { Text("Monto") })
 
+        Spacer(Modifier.height(16.dp))
+        Button(onClick = {
+            val dinero = Monto.toDoubleOrNull() ?: 0.0
+            if (dinero > 0) {
+                resultado = "Convirtiendo..."
+                investigador.launch(Dispatchers.IO) {
+                    try {
+                        val json = URL("https://api.frankfurter.app/latest?from=USD&to=EUR")
+                            .readText()
+                        val diferencia = JSONObject(json).getJSONObject("rates").getDouble("EUR")
+                        resultado = "%.2f USD = %.2f EUR".format(dinero, dinero * diferencia)
+                    } catch (e: Exception) {
+                        resultado = "Error al conectar "
+                    }
+                }
+            } else {
+                resultado = "Monto inválido"
+            }
+        }) {
+            Text("Convertir")
+        }
 
+        Spacer(Modifier.height(24.dp))
+        Text(resultado, fontSize = 20.sp)
     }
 }
